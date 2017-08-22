2ccf1561c7371ae21cb7d4eea669ab1acbec2715
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-22 17:21:22 +0200

    set the relationships between Airport and Model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+	has_many :flights, foreign_key: :start_airport_id
+	has_many :flights, foreign_key: :end_airport_id
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+	belongs_to :start_airport, 	class_name: "Airport"
+	belongs_to :end_airport,		class_name: "Airport"
 end
