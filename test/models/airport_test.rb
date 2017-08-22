371b5032fba1e80dbde8a59cff05e7ea0848fb89
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-22 17:20:45 +0200

    prepare the Airport model test

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@gal = Airport.create(name: "Gallipoli", icao: "LIBG")
+	end
+
+	test "it should be valid" do
+		assert @gal.valid?
+	end
 end
