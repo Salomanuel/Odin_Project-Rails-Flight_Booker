8dba233eaac142238d1ad0d6081c3598ac00e29c
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-22 14:17:35 +0200

    add the seeds for the Airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Airport::delete_all
+10.times do
+	Airport.create!(name:	Faker::Address.city )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
