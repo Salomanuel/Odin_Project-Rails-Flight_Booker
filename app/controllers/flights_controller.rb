3c93d721df9a76c30fb89c0c60163df2cbf9a5fc
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-23 18:56:15 +0200

    flight searches get returned with Departure and Arrival airport

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,16 @@
 class FlightsController<ApplicationController
 
 	def index
-		@flights  = Flight.all
 		@airports = Airport.all.map{ |a| [a.name, a.id] }
+		if params[:flight_query]
+			# @flight  = params[:flight_query]
+			@flights = Flight.where(
+				start_airport_id: params[:flight_query][:start_airport_id],
+				end_airport_id: 	params[:flight_query][:end_airport_id]
+				)
+
+			# Airport.all.find_by(id:@flight[:start_airport_id])
+		end
 		
 	end
 
