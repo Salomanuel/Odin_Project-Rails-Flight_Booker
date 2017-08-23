0773b3e7f7742a29b22f372521f79687659896bb
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-23 19:21:05 +0200

    add Time into seeds and improve table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,12 @@ end
 15.times do
 	start_airport = Airport.all.sample.id
 	end_airport   = Airport.all.sample.id
+	date          = Time.now + rand(5000000)
 	while start_airport == end_airport
 		end_airport = Airport.all.sample.id
 	end
 
 	Flight.create!(start_airport_id: start_airport,
-		             end_airport_id:   end_airport)
+		             end_airport_id:   end_airport,
+		             date: 						 date)
 end
