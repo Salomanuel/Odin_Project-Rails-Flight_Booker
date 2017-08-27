100732d5135f5df5692e4912438bd07c95301006
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-27 15:56:33 +0200

    improve Booking #new page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,16 @@
 class BookingsController < ApplicationController
 	def new
 		@booking = Booking.new
+		@flight  = Flight.find_by(:id[params[:booking][:flight_id]])
 	end
 
-	def create
-		@booking = Booking.new(booking_params)
-		if @booking.save
-			render html: "saved"
-		end
-	end
+	# def create
+	# 	@booking = Booking.new(booking_params)
+	# 	if @booking.save
+	# 		# render html: "saved"
+	# 		# render @booking
+	# 	end
+	# end
 
 	private
 		def booking_params
