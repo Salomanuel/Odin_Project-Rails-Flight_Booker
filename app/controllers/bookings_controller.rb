cac7de4ba5d570c94e72c923f033327538f83a6a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-27 13:02:55 +0200

    the bookings get through the first page!

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+	def new
+		@booking = Booking.new
+	end
+
+	def create
+		@booking = Booking.new(booking_params)
+		if @booking.save
+			render html: "saved"
+		end
+	end
+
+	private
+		def booking_params
+			params.require(:booking).permit(:flight_id)
+		end
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,13 +3,11 @@ class FlightsController<ApplicationController
 	def index
 		@airports = Airport.all.map{ |a| [a.name, a.id] }
 		if params[:flight_query]
-			# @flight  = params[:flight_query]
+			@booking = Booking.new
 			@flights = Flight.where(
 				start_airport_id: params[:flight_query][:start_airport_id],
 				end_airport_id: 	params[:flight_query][:end_airport_id]
 				)
-
-			# Airport.all.find_by(id:@flight[:start_airport_id])
 		end
 		
 	end
