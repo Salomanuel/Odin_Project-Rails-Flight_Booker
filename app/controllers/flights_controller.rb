e1b4b46a01b925aaf34330baf62fecd6c3ab3330
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-11 18:53:18 +0200

    set up a primordial form for searching Flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,8 @@ class FlightsController < ApplicationController
 	def index
 		@airports = Airport.all
 		@flights  = Flight.all
+
+		@airports_select = Airport.all.map{ |a| [a.name, a.id] }
 	end
 
 end
