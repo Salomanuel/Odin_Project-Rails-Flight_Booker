11b6058eb587551c2928579bb7196b2cf0a6b755
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-11 16:17:56 +0200

    seeds for Airports and Flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airport::delete_all
+Flight::delete_all
+
+cities = ["Roma", "Paris", "London", "Berlin", "Madrid"].sort
+
+cities.each do |city|
+	Airport.create(name: city)
+end
+
+def random_airport(airport = nil)
+	other_airport = Airport.all[rand(Airport.all.count)]
+	if other_airport == airport
+		random_airport(airport) 
+	else 
+		return other_airport
+	end
+end
+
+50.times do |i|
+	flight = Flight.new
+	flight.departure_airport = random_airport.id
+	flight.arrival_airport   = random_airport(Airport.find(flight.departure_airport)).id
+	flight.date = Time.now + rand(1000000).seconds
+	flight.save
+end
+
