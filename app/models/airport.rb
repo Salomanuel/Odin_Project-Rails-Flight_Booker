20dd388898dd715565791caf22e0f8a60092461a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-11 18:38:57 +0200

    set relationships in Airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,8 @@
 class Airport < ApplicationRecord
+	has_many :departures,
+		foreign_key: "departure_airport",
+		class_name: "Flight"
+	has_many :arrivals,
+		foreign_key: "arrival_airport",
+		class_name: "Flight"
 end
