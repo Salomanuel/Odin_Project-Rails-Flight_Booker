460195089f24072c2c0dd1d6953da0c6633e3910
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-12 18:21:01 +0200

    implement the days with Flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,12 +6,15 @@ class FlightsController < ApplicationController
 	end
 
 	def search
-		@flights ||= Array.new
 		@airports_select = Airport.all.map{ |a| [a.name, a.id] }
+
+		@flight_days = Flight.all.group_by{ |x| x.date.strftime("%a, %b %d %Y") }	
+
 		if params[:submitted]
 			@flights = Flight.where("departure_airport = ? AND arrival_airport = ?",
 				params[:departure_airport], params[:arrival_airport])
 		end
+	
 		@booking = "ok"
 	end
 
