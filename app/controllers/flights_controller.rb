23e5281ed93599972e71175824389b0e54d5a469
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-12 10:38:33 +0200

    implement params[:submitted] to hide or not the Flights results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,7 @@ class FlightsController < ApplicationController
 	def search
 		@flights ||= Array.new
 		@airports_select = Airport.all.map{ |a| [a.name, a.id] }
-		if params[:arrival_airport]
+		if params[:submitted]
 			@flights = Flight.where("departure_airport = ? AND arrival_airport = ?",
 				params[:departure_airport], params[:arrival_airport])
 		end
