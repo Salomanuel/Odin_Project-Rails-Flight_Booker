e7a19e996b14e217dbf70a05ec6d4b8902022be6
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-13 16:37:24 +0200

    add Text Helpers for pluralize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  include ActionView::Helpers::TextHelper
   protect_from_forgery with: :exception
+
 
   def hello
   	render inline: "<h1>hello</h1>"
