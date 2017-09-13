443c52c6d07821e7eaefd4907897830a18b60fc0
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-13 12:03:47 +0200

    the Flight search works with arrival, departure and time

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -11,11 +11,24 @@ class FlightsController < ApplicationController
 		@flight_days = Flight.all.map{ |x| x.date.strftime("%a, %b %d %Y") }.uniq
 
 		if params[:submitted]
-			@flights = Flight.where("departure_airport = ? AND arrival_airport = ?",
-				params[:departure_airport], params[:arrival_airport])
+			@flights = find_flight(params[:date], 
+														 params[:departure_airport], 
+														 params[:arrival_airport]    )
+			# Flight.where("departure_airport = ? AND arrival_airport = ?",
+				# params[:departure_airport], params[:arrival_airport])
 		end
 	
 		@booking = "ok"
 	end
+
+	private
+		def find_flight(my_date, departure, arrival)
+			Flight.find_by_sql [ "SELECT * FROM flights 
+				WHERE departure_airport = ? 
+				AND arrival_airport = ?
+				AND strftime('%d %m %Y', date) = ?", 
+				departure, arrival,
+				Time.parse(my_date).strftime('%d %m %Y')]
+		end
 
 end
