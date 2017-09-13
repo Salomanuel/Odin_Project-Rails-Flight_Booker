1f56070be59da6bf14c111984f5d4c01ece03458
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-13 12:15:40 +0200

    refactor the arguments from #find_flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -9,23 +9,17 @@ class FlightsController < ApplicationController
 		@airports_select = Airport.all.map{ |a| [a.name, a.id] }
 		@flight_days = Flight.order(:date).map{ |x| x.date.strftime("%a, %b %d %Y") }.uniq
 		@booking = "ok"
-		if params[:submitted]
-			@flights = find_flight(params[:date], 
-														 params[:departure_airport], 
-														 params[:arrival_airport]    )
-		end
-	
-		
+		@flights = find_flight(params) if params[:submitted]
 	end
 
 	private
-		def find_flight(my_date, departure, arrival)
+		def find_flight(params) #(my_date, departure, arrival)
 			Flight.find_by_sql [ "SELECT * FROM flights 
 				WHERE departure_airport = ? 
 				AND arrival_airport = ?
 				AND strftime('%d %m %Y', date) = ?", 
-				departure, arrival,
-				Time.parse(my_date).strftime('%d %m %Y')]
+					params["departure_airport"], 
+					params["arrival_airport"],
+					Time.parse(params["date"]).strftime('%d %m %Y')]
 		end
-
 end
