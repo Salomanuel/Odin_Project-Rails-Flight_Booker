8d7f30c5fd1f04667069c0b84195096a69b15acd
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-16 16:51:48 +0200

    write basic PassengerMailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+	default from: 'manuel@example.com'
+
+	def welcome_email(user)
+		@user = user
+		@url  = 'http://example.com/login'
+		mail(to: @user.email, subject: 'Welcome mate')
+	end
 end
