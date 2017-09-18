f7c05811e52e2a23ddab8bf74f6d416b8c3c022e
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-18 11:51:31 +0200

    Mario fixes are fixed!
    - implement #same_airport? and a message to notify the user that the
      departure and arrival airport coincide
    
    - remember fields after the search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,14 +6,28 @@ class FlightsController < ApplicationController
 	end
 
 	def search
-		@airports_select = Airport.all.map{ |a| [a.name, a.id] }
+		@airports_select   = Airport.all.map{ |a| [a.name, a.id] }
 		@flight_days = Flight.order(:date).map{ |x| x.date.strftime("%a, %b %d %Y") }.uniq
+
+		@departure_airport ||= @airports_select[0]
+		@arrival_airport   ||= @airports_select[1]
+		@date							 ||= @flight_days[0]
+
 		@booking = Booking.new
 		@flights = find_flight(params) if params[:submitted]
 	end
 
+	def same_airports?
+		return true if params["departure_airport"] == params["arrival_airport"]
+		return false
+	end
+	helper_method :same_airports?
+
 	private
 		def find_flight(params) #(my_date, departure, arrival)
+			@departure_airport = params[:departure_airport]
+			@arrival_airport   = params[:arrival_airport]
+			@date							 = params[:date]
 			Flight.find_by_sql [ "SELECT * FROM flights 
 				WHERE departure_airport 			 	 = ? 
 				AND 	arrival_airport 					 = ?
@@ -24,4 +38,5 @@ class FlightsController < ApplicationController
 					Time.parse(params["date"]).strftime('%d').to_i,
 				  Time.parse(params["date"]).strftime('%m').to_i]
 		end
+
 end
