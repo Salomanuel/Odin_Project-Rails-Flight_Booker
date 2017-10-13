fb6fa2e90fa9db7ff6f31483e08e5c96400e328d
Salomanuel <salomanuel.aux+code@gmail.com>
2017-10-13 10:22:23 +0200

    still fighting against the static assets and heroku-2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,4 @@
 Rails.application.configure do
-  config.serve_static_assets = true
-  config.assets.compile = true
-
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -92,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.serve_static_files = true
+  config.assets.compile = true
 end
